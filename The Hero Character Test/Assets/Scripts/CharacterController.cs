e875504c8f187112de91e7e8175505585a1aa7fb
Roddymal <cv9ruleZ!>
2018-08-21 22:08:02 +0200

    Character Controller Update
    The first itteration of the 3rd person camera is present

---
+++ The Hero Character Test/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    float directionDampTime = .25f;
+
+    float speed = 0;
+    float h = 0;
+    float v = 0;
+
+	
+    
+    // Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        if(animator.layerCount >= 2)
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animator)
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            speed = new Vector2(h, v).sqrMagnitude;
+
+            animator.SetFloat("Speed", speed);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+	}
+}
---
+++ The Hero Character Test/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    [SerializeField]
+    float distanceAway;
+    [SerializeField]
+    float distanceUp;
+    [SerializeField]
+    float smooth;
+    [SerializeField]
+    Transform follow;
+    Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+        follow = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        transform.LookAt(follow);
+    }
+}
