23830ce50f2ab5a630447b613ff628e432962d38
Roddymal <cv9ruleZ!>
2018-08-21 12:06:12 +0200

    Character Movement v1 no camera no test Body
    This is me setting up the Character to prepare for the camera and sprinting

---
+++ Rigged Character controller/Assets/Locomotion_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion_Movement : StateMachineBehaviour {
+
+    public float Damping = 0.15f;
+
+    private readonly int HashHorizontalParamater = Animator.StringToHash("Horizontal");
+    private readonly int HashVerticalParamater = Animator.StringToHash("Vertical");
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 input = new Vector2(horizontal, vertical).normalized;
+
+        animator.SetFloat(HashHorizontalParamater, input.x, Damping, Time.deltaTime);
+        animator.SetFloat(HashVerticalParamater, input.y, Damping, Time.deltaTime);
+
+    }
+}
