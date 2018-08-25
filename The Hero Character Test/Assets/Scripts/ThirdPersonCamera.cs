2986fb479ff80bd27fdfc8c575911fd5303f4fe2
Roddymal <cv9ruleZ!>
2018-08-25 18:39:52 +0200

    Camera V2 updated
    the camera and animations have been improved significantly.

--- The Hero Character Test/Assets/Scripts/ThirdPersonCamera.cs
+++ The Hero Character Test/Assets/Scripts/ThirdPersonCamera.cs
@@ -11,27 +11,53 @@ public class ThirdPersonCamera : MonoBehaviour {
     [SerializeField]
     float smooth;
     [SerializeField]
-    Transform follow;
+    Transform followXform;
+    [SerializeField]
+    Vector3 offset = new Vector3(0f, 1f, 0f);
+
     Vector3 targetPosition;
-	// Use this for initialization
-	void Start () {
-        follow = GameObject.FindWithTag("Player").transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector3 lookDir;
+
+
+    Vector3 velocityCanSmooth = Vector3.zero;
+    [SerializeField]
+    float canSmoothDampTime = .1f;
+    
+    // Use this for initialization
+    void Start() {
+        followXform = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     private void LateUpdate()
     {
-        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
-        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
-        Debug.DrawRay(follow.position, -1f * follow.forward, Color.blue);
-        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        Vector3 characterOffset = followXform.position + offset;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        lookDir = characterOffset - this.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        Debug.DrawRay(this.transform.position, lookDir, Color.green);
 
-        transform.LookAt(follow);
+
+        targetPosition = characterOffset + followXform.up * distanceUp - lookDir * distanceAway;
+        Debug.DrawRay(followXform.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(followXform.position, -1f * followXform.forward, Color.blue);
+        Debug.DrawLine(followXform.position, targetPosition, Color.magenta);
+
+       
+        smoothPosition(this.transform.position, targetPosition);
+       
+
+        transform.LookAt(followXform);
     }
+
+    void smoothPosition (Vector3 fromPos, Vector3 toPos)
+    {
+        this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCanSmooth, canSmoothDampTime);
+    }
+
 }
