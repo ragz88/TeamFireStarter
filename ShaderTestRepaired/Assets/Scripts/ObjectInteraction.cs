aa228512f085e8009566cfc62c73c200c4f6bd48
Erik Prinz <erik88d3@gmail.com>
2018-08-26 14:57:57 +0200

    Uploading test scene
    This test scene includes a temporary character controller that can lift blocks and push other blocks. It has moving tings and energy paths.

---
+++ ShaderTestRepaired/Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    public string[] pickupTags;
+    public GameObject prompt;
+    public Transform holdPos;
+    public float pickUpSpeed = 0.1f;
+
+    public bool nextToPickup = false;
+    public bool nextToPushable = false;
+    public bool holdingPickup = false;
+    public bool pushingObject = false;
+    public bool lerpingHeldBlock = false;
+    public bool droppingPickup = false;
+
+    public const float bufferDist = 0.75f;
+
+    public GameObject pickupObj;
+    public GameObject pushObject;
+
+    ThirdPersonUserControl characterControl;
+
+    // Use this for initialization
+    void Start () {
+        characterControl = GameObject.Find("ThirdPersonController").GetComponent<ThirdPersonUserControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pushingObject)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !droppingPickup)
+            {
+                pushingObject = false;
+                droppingPickup = true;
+                characterControl.isPushing = false;
+                pushObject.GetComponent<Rigidbody>().mass = 1000;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && nextToPushable && !holdingPickup && !pushingObject && !droppingPickup)
+        {
+            pushingObject = true;
+            characterControl.isPushing = true;
+            pushObject.GetComponent<Rigidbody>().mass = 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && holdingPickup)
+        {
+            holdingPickup = false;
+            pickupObj.GetComponent<Rigidbody>().useGravity = true;
+            pickupObj.GetComponent<Rigidbody>().isKinematic = false;
+            droppingPickup = true;
+            lerpingHeldBlock = false;
+            //Invoke("endDrop", 0.1f);
+        }
+
+        if (nextToPickup && !holdingPickup && !droppingPickup && !pushingObject)
+        {
+            prompt.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                holdingPickup = true;
+                pickupObj.GetComponent<Rigidbody>().useGravity = false;
+                pickupObj.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+
+   
+        if (holdingPickup)
+        {
+            prompt.SetActive(false);
+            if (Vector3.Distance(holdPos.position, pickupObj.transform.position) > 0.05f && holdingPickup)
+            {
+                lerpingHeldBlock = true;
+            }
+            else
+            {
+                lerpingHeldBlock = false;
+            }
+
+        }
+
+        if (lerpingHeldBlock)
+        {
+            pickupObj.transform.position = Vector3.Lerp(pickupObj.transform.position, holdPos.position, pickUpSpeed);
+        }
+
+
+        droppingPickup = false;
+
+        nextToPickup = false;
+        nextToPushable = false;
+        //prompt.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider hit)
+    {
+        if (!holdingPickup)
+        {
+            bool foundPickUp = false;
+            for (int i = 0; i < pickupTags.Length; i++)
+            {
+                if (hit.tag == pickupTags[i])
+                {
+                    nextToPickup = true;
+                    prompt.SetActive(true);
+                    foundPickUp = true;
+                    pickupObj = hit.gameObject;
+                }
+                /*if (foundPickUp)
+                {
+                    prompt.SetActive(true);
+                    nextToPickup = true;
+                }
+                else
+                {
+                    prompt.SetActive(false);
+                    nextToPickup = false;
+                }*/
+            }
+
+            if (!foundPickUp && hit.tag == "Pushable")
+            {
+                nextToPushable = true;
+                print("boop");
+                pushObject = hit.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider hit)
+    {
+        if (!holdingPickup)
+        {
+            for (int i = 0; i < pickupTags.Length; i++)
+            {
+                if (hit.tag == pickupTags[i])
+                {
+                    prompt.SetActive(false);
+                    nextToPickup = false;
+                }
+            }
+        }
+    }
+
+}
---
+++ ShaderTestRepaired/Assets/Scripts/ObjectInteractions.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ObjectInteractions : MonoBehaviour {
+
+    //public string[] pickupTags;
+    public GameObject prompt;
+    public Transform liftPos;
+    public Transform pushPos;
+    public float liftSpeed = 0.1f;
+
+    bool nextToPickup = false;
+    bool nextToPushable = false;
+    public bool holdingPickup = false;
+    public bool pushingObject = false;
+
+
+    public bool lerpingPickup = false;
+    //public bool droppingPickup = false;
+    bool transitioning = false;  //true on the first frame when e is pressed
+
+    public const float bufferDist = 0.75f;
+
+    public GameObject objectToLift;
+    public GameObject objectToPush;
+
+    public LayerMask rayMask;
+
+    ThirdPersonUserControl characterControl;
+
+    Text promptText;
+
+    // Use this for initialization
+    void Start () {
+        promptText = prompt.GetComponent<Text>();
+        characterControl = gameObject.GetComponentInParent<ThirdPersonUserControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        PushableObject pushScript;
+        LiftableObject liftScript;
+
+        RaycastHit rayHit;
+        if (Physics.Raycast(transform.position + new Vector3(0, 1, 0), transform.forward + new Vector3(0, -0.5f, 0), out rayHit, 0.75f, rayMask))
+        {
+            pushScript = rayHit.collider.gameObject.GetComponent<PushableObject>();
+            liftScript = rayHit.collider.gameObject.GetComponent<LiftableObject>();
+
+            if (liftScript != null)
+            {
+                objectToLift = rayHit.collider.gameObject;
+                nextToPickup = true;
+            }
+            else
+            {
+                if (!holdingPickup)
+                {
+                    objectToLift = null;
+                }
+
+                if (pushScript != null)
+                {
+                    objectToPush = rayHit.collider.gameObject;
+                    nextToPushable = true;
+                }
+                else
+                {
+                    if (!pushingObject)
+                    {
+                        objectToPush = null;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (!holdingPickup)
+            {
+                objectToLift = null;
+            }
+            nextToPickup = false;
+            if (!pushingObject)
+            {
+                objectToPush = null;
+            }
+            nextToPushable = false;
+        }
+        Debug.DrawRay(transform.position + new Vector3(0, 1, 0), (transform.forward + new Vector3(0, -0.5f, 0)).normalized*0.75f, Color.magenta);
+        
+
+        if (nextToPickup && !holdingPickup && !pushingObject)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //lerpingPickup = true;
+                holdingPickup = true;
+                objectToLift.GetComponent<Rigidbody>().useGravity = false;
+                objectToLift.GetComponent<Rigidbody>().isKinematic = true;
+                transitioning = true;
+            }
+        }
+
+        if (nextToPushable && !holdingPickup && !pushingObject)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                transitioning = true;
+                pushingObject = true;
+                characterControl.isPushing = true;
+                //objectToPush.GetComponent<Rigidbody>().mass = 5;
+                //objectToPush.GetComponent<Rigidbody>().isKinematic = true;
+                Physics.IgnoreCollision(objectToPush.GetComponent<Collider>(), gameObject.transform.parent.GetComponent<Collider>());
+                pushPos.position = objectToPush.transform.position;
+            }
+        }
+
+        if (holdingPickup && !transitioning)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //lerpingPickup = false; 
+                holdingPickup = false;
+                objectToLift.GetComponent<Rigidbody>().isKinematic = false;
+                objectToLift.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+
+        if (pushingObject && !transitioning)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pushingObject = false;
+                characterControl.isPushing = false ;
+                //objectToPush.GetComponent<Rigidbody>().mass = 1000;
+                Physics.IgnoreCollision(objectToPush.GetComponent<Collider>(), gameObject.transform.parent.GetComponent<Collider>(), false);
+            }
+        }
+
+        if (holdingPickup)
+        {
+            objectToLift.transform.position = Vector3.Lerp(objectToLift.transform.position, liftPos.position, liftSpeed);
+        }
+
+        if (pushingObject)
+        {
+            objectToPush.transform.position = Vector3.Lerp(objectToPush.transform.position, pushPos.position, 5);
+        }
+
+        transitioning = false;
+    }
+}
